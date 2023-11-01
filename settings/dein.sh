490185737d7800acb7bc8423dd967faf6c5d3470
hulk510 <dorian.51069@gmail.com>
2023-11-02 01:05:24 +0900

    weztermとneovim入れたいのでvimrc, tmux, dein削除した

--- settings/dein.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
-# For example, we just use `~/.cache/dein` as installation directory
-sh ./installer.sh ~/.cache/dein
-
-# インストールするとinstaller.shができるのでそれは削除してもいい
-# 終わったらvim開いて↓を呼べばインストール完了
-# :call dein#install()
