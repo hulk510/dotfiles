9d78a94089a1bc9dd5b8db2970848af57767b59e
hulk510 <dorian.51069@gmail.com>
2024-04-03 23:15:33 +0900

    telescopeでgitのプロジェクトの場合はプロジェクトルートから検索するように修正

--- dot_config/nvim/lua/keymaps/telescope.lua
+++ dot_config/nvim/lua/keymaps/telescope.lua
@@ -1,11 +1,15 @@
 local M = {}
+local builtin = require('telescope.builtin')
 
 M.setup = function()
-  local builtin = require('telescope.builtin')
-  vim.keymap.set('n', '<leader>ff', builtin.find_files, { silent = true })
-  vim.keymap.set('n', '<leader>fg', builtin.live_grep, { silent = true })
+  -- vim.keymap.set('n', '<leader>ff', builtin.find_files, { silent = true })
+  --  vim.keymap.set('n', '<leader>fg', builtin.live_grep, { silent = true })
   vim.keymap.set('n', '<leader>fb', builtin.buffers, { silent = true })
   vim.keymap.set('n', '<leader>fh', builtin.help_tags, { silent = true })
+  vim.keymap.set("n", "<Leader><Leader>", "<CMD>lua require'plugins.telescope'.project_files()<CR>",
+    { noremap = true, silent = true })
+  vim.keymap.set("n", "<Leader>g", "<CMD>lua require'plugins.telescope'.project_grep()<CR>",
+    { noremap = true, silent = true })
 end
 
 return M
