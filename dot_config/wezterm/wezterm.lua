a6d0784b3d6045ea47561747d30b4f2a7e0489c3
hulk510 <dorian.51069@gmail.com>
2025-11-14 14:11:16 +0900

    feat: add starship prompt configuration and install additional brew packages

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,30 +1,21 @@
--- Pull in the wezterm API
 local wezterm = require 'wezterm'
-
--- This table will hold the configuration.
 local config = {}
 
--- In newer versions of wezterm, use the config_builder which will
--- help provide clearer error messages
 if wezterm.config_builder then
   config = wezterm.config_builder()
 end
 
--- This is where you actually apply your config choices
-config.font = wezterm.font 'Fira Code'
+-- フォント設定（Nerd Font対応）
+config.font = wezterm.font 'Hack Nerd Font Mono'
 config.font_size = 16.0
 
--- For example, changing the color scheme:
+-- カラースキーム
 config.color_scheme = 'tokyonight'
 
--- 一つのタブだったらtab_barを表示しない。
+-- タブバー設定
 config.hide_tab_bar_if_only_one_tab = true
--- 背景の透過度
- config.window_background_opacity = 0.9
--- config.background = {
---  {
---    opacity = 0.5
---  }
--- }
--- and finally, return the configuration to wezterm
+
+-- 背景透過度
+config.window_background_opacity = 0.9
+
 return config
